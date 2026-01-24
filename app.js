2534cd201da4ff6c3bdfa024a21418fef8bf10da
Marck <myohalmoam@gmail.com>
2026-01-24 08:47:42 -0600

    added user input function

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-function getComputerChoice() {
-    let rand_choice = Math.floor(Math.random() * 3) + 1
-    return rand_choice
+const getComputerChoice = function() {
+    let rand_choice = Math.floor(Math.random() * 3) + 1;
+
+    if (rand_choice === 1) {
+        return "rock"
+    } else if (rand_choice === 2) {
+        return "paper"
+    }else {
+        return "scissors"
+    }
 }
 
 console.log(getComputerChoice())
+
+const getHumanChoice = function(str) {
+    let user_input = prompt("Please choose between --- Rock Paper Scissors")
+    user_input = user_input.toLowerCase();
+
+    return user_input
+}
+
